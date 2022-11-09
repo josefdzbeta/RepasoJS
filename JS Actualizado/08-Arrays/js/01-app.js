9dc0ed0378f0c81d6be6f4ec75887d34dcc153fd
josefdzbeta <jafb19@outlook.com>
2022-11-09 19:30:21 +0100

    work

---
+++ JS Actualizado/08-Arrays/js/01-app.js
@@ -0,0 +1,17 @@
+//Arrays
+//1
+const numeros = [10,20,30]
+
+//2
+const meses = new Array('Enero', 'Febrero', 'Marzo')
+
+console.log(numeros);
+console.log(meses);
+
+//Array que contiene datos de todo tipo
+
+const deTodo = ['Hola',10,true, 'si', null, {nombre: 'Jose', trabajo: 'Programador'}, [1,2,3]]
+
+console.log(deTodo);
+
+
