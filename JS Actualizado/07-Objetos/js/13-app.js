dc47e0c658e1fed904ddb184a76e44f76d78e9d8
josefdzbeta <jafb19@outlook.com>
2022-11-09 20:46:37 +0100

    map

---
+++ JS Actualizado/07-Objetos/js/13-app.js
@@ -0,0 +1,14 @@
+const producto = {
+    nombre: 'monitor de 20 pulgadas',
+    precio: 300,
+    disponible: true,
+}
+
+//Obtener llaves de un objeto
+console.log(Object.keys(producto));
+
+//Obtener los valores del objeto
+console.log(Object.values(values));
+
+//Obtener llaves y valores
+console.log(Object.entries(producto));
