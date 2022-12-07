608db4716f020c4ceb343ec8570f1cb76a25c9a6
josefdzbeta <jafb19@outlook.com>
2022-12-07 13:24:12 +0100

    Async Await Pixabay

--- 42-PROYECTO-AsyncAwait-Pixabay/js/app.js
+++ 42-PROYECTO-AsyncAwait-Pixabay/js/app.js
@@ -48,19 +48,28 @@ function mostrarAlerta(mensaje) {
 
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
-    const key = '';
+    const key = '31824754-7322f30b9c0e926234f5346c8';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
+    // fetch(url)
+    //     .then(respuesta => respuesta.json())
+    //     .then(resultado => {
+    //         totalPaginas = calcularPaginas(resultado.totalHits);
+    //         mostrarImagenes(resultado.hits);
+    //     });
+
+        try {
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
             totalPaginas = calcularPaginas(resultado.totalHits);
             mostrarImagenes(resultado.hits);
-        });
+        } catch (error) {
+            console.log(error);
+        }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
